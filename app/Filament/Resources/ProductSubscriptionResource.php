85e7a1692d66c975e1183ab5d958e7368d5f6c52
iNd0n3s1A_cyb3R_Cr1M3 <djakarta1973@gmail.com>
2024-12-28 13:08:42 +0700

    feat: Implement ProductSubscriptionResource for transaction management
    - Developed the `ProductSubscriptionResource` class to manage CRUD operations for product subscriptions, including product selection, customer information, and payment details.
    - Integrated a multi-step wizard form for smooth data entry, with separate steps for selecting product and pricing, entering customer details, and providing payment information.
    - Added dynamic calculation for the total amount and tax based on the selected product, updating in real-time upon product selection.
    - Enabled the `is_paid` toggle button with icon representations to mark transaction payment status, and added proof upload functionality to handle payment evidence.
    - Created a table for displaying transaction data, featuring searchable columns for product name, booking transaction ID, and payment status, along with custom icons for visual feedback.
    - Implemented filters for filtering by product and trashed records in the table.
    - Added a custom action to approve transactions, updating the `is_paid` field and sending a notification to confirm approval.
    - Configured bulk actions for deleting, restoring, or permanently deleting multiple transactions at once, enhancing administrative control.
    - Included soft delete functionality, excluding soft-deleted records from the default query for better management of trashed data.
    
    This commit establishes a robust transaction management system using Filament, including dynamic form handling, payment status tracking, and efficient data display in the admin panel.

--- app/Filament/Resources/ProductSubscriptionResource.php
+++ app/Filament/Resources/ProductSubscriptionResource.php
@@ -4,26 +4,135 @@ namespace App\Filament\Resources;
 
 use App\Filament\Resources\ProductSubscriptionResource\Pages;
 use App\Filament\Resources\ProductSubscriptionResource\RelationManagers;
+use App\Models\Product;
 use App\Models\ProductSubscription;
 use Filament\Forms;
+use Filament\Forms\Components\Grid;
+use Filament\Forms\Components\ToggleButtons;
+use Filament\Tables\Filters\SelectFilter;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
 use Filament\Tables;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Illuminate\Support\Facades\Notification;
 
 class ProductSubscriptionResource extends Resource
 {
     protected static ?string $model = ProductSubscription::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static ?string $navigationIcon = 'heroicon-o-credit-card';
+
+    protected static ?string $navigationGroup = 'Transactions';
 
     public static function form(Form $form): Form
     {
         return $form
             ->schema([
-                //
+                Forms\Components\Wizard::make([
+                    Forms\Components\Wizard\Step::make('Product and Price')
+                        ->schema([
+                            Grid::make(2)->schema([
+                                Forms\Components\Select::make('product_id')
+                                    ->relationship('product', 'name')
+                                    ->searchable()
+                                    ->preload()
+                                    ->required()
+                                    ->live()
+                                    ->afterStateUpdated(function ($state, callable $set) {
+                                        $product = Product::find($state);
+                                        $price = $product ? $product->price_per_person : 0;
+                                        $duration = $product ? $product->duration : 0;
+
+                                        $set('price', $price);
+                                        $set('duration', $duration);
+
+                                        $tax = 0.11;
+                                        $totalTaxAmount = $tax * $price;
+                                        $totalAmount = $price + $totalTaxAmount;
+
+                                        $set('total_amount', number_format($totalAmount, 0, '', ''));
+                                        $set('total_tax_amount', number_format($totalTaxAmount, 0, '', ''));
+                                    }),
+
+                                Forms\Components\TextInput::make('price')
+                                    ->required()
+                                    ->label('Price per person')
+                                    ->readOnly()
+                                    ->numeric()
+                                    ->prefix('IDR'),
+
+                                Forms\Components\TextInput::make('total_amount')
+                                    ->required()
+                                    ->readOnly()
+                                    ->numeric()
+                                    ->prefix('IDR'),
+
+                                Forms\Components\TextInput::make('total_tax_amount')
+                                    ->readOnly()
+                                    ->required()
+                                    ->numeric()
+                                    ->prefix('IDR'),
+
+                                Forms\Components\TextInput::make('duration')
+                                    ->readOnly()
+                                    ->required()
+                                    ->numeric()
+                                    ->prefix('Month'),
+                            ]),
+                        ]),
+                    Forms\Components\Wizard\Step::make('Customer Information')
+                        ->schema([
+                            Grid::make(2)->schema([
+                                Forms\Components\TextInput::make('name')
+                                    ->required()
+                                    ->maxLength(255),
+
+                                Forms\Components\TextInput::make('phone')
+                                    ->required()
+                                    ->maxLength(255),
+
+                                Forms\Components\TextInput::make('email')
+                                    ->required()
+                                    ->maxLength(255),
+                            ]),
+                        ]),
+                    Forms\Components\Wizard\Step::make('Payment Information')
+                        ->schema([
+                            Forms\Components\TextInput::make('booking_trx_id')
+                                ->required()
+                                ->maxLength(255),
+
+                            Forms\Components\TextInput::make('customer_bank_name')
+                                ->required()
+                                ->maxLength(255),
+
+                            Forms\Components\TextInput::make('customer_bank_account')
+                                ->required()
+                                ->maxLength(255),
+
+                            Forms\Components\TextInput::make('customer_bank_number')
+                                ->required()
+                                ->maxLength(255),
+                            ToggleButtons::make('is_paid')
+                                ->label('Apakah sudah membayar?')
+                                ->boolean()
+                                ->grouped()
+                                ->icons([
+                                    true => 'heroicon-o-pencil',
+                                    false => 'heroicon-o-clock',
+                                ])
+                                ->required(),
+
+                            Forms\Components\FileUpload::make('proof')
+                                ->image()
+                                ->required(),
+                        ]),
+                ])
+                    ->columnSpan('full') // Use full width for the wizard
+                    ->columns(1) // Make sure the form has a single column layout
+                    ->skippable(),
             ]);
     }
 
@@ -31,13 +140,46 @@ class ProductSubscriptionResource extends Resource
     {
         return $table
             ->columns([
-                //
+                Tables\Columns\ImageColumn::make('product.thumbnail'),
+
+                Tables\Columns\TextColumn::make('name')
+                    ->searchable(),
+
+                Tables\Columns\TextColumn::make('booking_trx_id')
+                    ->searchable(),
+
+                Tables\Columns\IconColumn::make('is_paid')
+                    ->boolean()
+                    ->trueColor('success')
+                    ->falseColor('danger')
+                    ->trueIcon('heroicon-o-check-circle')
+                    ->falseIcon('heroicon-o-x-circle')
+                    ->label('Terverifikasi'),
             ])
             ->filters([
+                SelectFilter::make('product_id')
+                    ->label('Product')
+                    ->relationship('product', 'name'),
                 Tables\Filters\TrashedFilter::make(),
             ])
             ->actions([
+                Tables\Actions\ViewAction::make(),
                 Tables\Actions\EditAction::make(),
+                Tables\Actions\Action::make('approve')
+                    ->label('Approve')
+                    ->action(function (ProductSubscription $record) {
+                        $record->is_paid = true;
+                        $record->save();
+
+                        Notification::make()
+                            ->title('Order Approved')
+                            ->success()
+                            ->body('The order has been successfully approved.')
+                            ->send();
+                    })
+                    ->color('success')
+                    ->requiresConfirmation()
+                    ->visible(fn(ProductSubscription $record) => !$record->is_paid),
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
