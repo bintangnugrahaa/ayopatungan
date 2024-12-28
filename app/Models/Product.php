fe8e1342dc6bec80fef3765a11da0797d171402b
iNd0n3s1A_cyb3R_Cr1M3 <djakarta1973@gmail.com>
2024-12-28 11:01:35 +0700

    feat: Configure fillable properties and relationships for models
    - Defined the `fillable` properties for the `Product`, `ProductKeypoint`, `ProductSubscription`, `SubscriptionGroup`, `GroupParticipant`, and `GroupMessage` models to protect against mass-assignment vulnerabilities and streamline data insertion.
    - Set up the one-to-many relationship between `Product` and `ProductKeypoint`, allowing easy access to related keypoints for each product.
    - Established a many-to-many relationship between `Product` and `ProductSubscription`, ensuring proper handling of product-subscription associations.
    - Configured the one-to-many relationship between `SubscriptionGroup` and `GroupParticipant`, enabling easy retrieval of participants in each subscription group.
    - Defined the one-to-many relationship between `GroupParticipant` and `GroupMessage`, facilitating message tracking per participant within a group.
    - Implemented the inverse relationships to ensure data consistency and proper eager loading in queries.
    
    This commit enhances the models by securing the data handling and defining relationships to improve database interactions and performance.

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,9 +2,47 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Str;
+use Illuminate\Database\Eloquent\Casts\Attribute;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Product extends Model
 {
-    //
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'slug',
+        'name',
+        'thumbnail',
+        'photo',
+        'about',
+        'tagline',
+        'price',
+        'duration',
+        'capacity',
+        'is_popular',
+        'price_per_person',
+    ];
+
+    protected function name(): Attribute
+    {
+        return Attribute::make(
+            set: fn(string $value) => [
+                'name' => $value,
+                'slug' => Str::slug($value)
+            ]
+        );
+    }
+    public function groups(): HasMany
+    {
+        return $this->hasMany(SubscriptionGroup::class);
+    }
+
+    public function keypoints(): HasMany
+    {
+        return $this->hasMany(ProductKeypoint::class);
+    }
 }
